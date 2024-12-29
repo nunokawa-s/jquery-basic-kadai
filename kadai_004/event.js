8985053b8d18f1f1d39672e0579df8b87015a70f
nunokawa-s <momenn54@gmail.com>
2024-12-29 18:36:36 +0900

    Delete kadai_004 directory

--- kadai_004/event.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-  $(window).on('scroll', function () {
-    console.log('scrollイベントが発生しました');
-  });
-});
-
-$(window).on('load', function () {
-  console.log('loadイベントが発生しました');
-});
